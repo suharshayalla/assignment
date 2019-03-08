33d67f91f7ef84e46ff571505848ebe260ad26b7
suharshayalla <46210454+suharshayalla@users.noreply.github.com>
2019-03-08 17:30:21 +0530

    Update simpleintrest.sh

--- ass1/simpleintrest.sh
+++ ass1/simpleintrest.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-echo "Enter Amount:"
-read p
-echo "Enter Time:"
-read t
-echo "Enter ROI:"
-read r
+p=$1
+t=$2
+r=$3
 s=` expr $p \* $t`
 i=`expr $s \* $r`  
 c=`expr $i / 100`
- 
 echo "Simple Interest is: $c"
