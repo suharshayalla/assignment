b65de94e4f71a3851c2c26779b99959d9f7fe255
suharshayalla <46210454+suharshayalla@users.noreply.github.com>
2019-03-08 22:14:38 +0530

    Update aapend.sh

--- ass8/aapend.sh
+++ ass8/aapend.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-echo " enter filename1 "
-read filename1
-echo " enter filename2 "
-read filename2
+filename1=$1
+filename2=$2
 if [ -f $filename1 -a -f $filename2 ]
 then 
 cat $filename1 >> $filename2
